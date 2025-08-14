91ec73de1872f76e5017d417dd817049f0928751
John <jhonhide007@gmail.com>
2025-08-14 15:50:35 +0000

    Operations team

---
+++ devops/Salesforce/Salesforce.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Geeks {
+    public static void main(String[] args)
+    {
+        int n = 10;
+        int m = 0;
+
+        int ans = n / m;
+
+        System.out.println("Answer: " + ans);
+    }
+}
